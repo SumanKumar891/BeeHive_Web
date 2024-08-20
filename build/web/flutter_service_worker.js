a52e4b8b306320b94fd4e166ed7eb6320723a3e4
Sejal-08 <sejalsankhyan2001@gmail.com>
2024-08-20 14:19:43 +0530

    mm

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "912bedf8e00a4d4a837e4a5365d9d64a
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "38dbad19fbc48e1b798e9ce5ec5141cc",
+"flutter_bootstrap.js": "ac2625ecafe4426aef1772504485b2b8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
