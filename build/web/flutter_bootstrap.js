3bfc8c606b9d97ffcaf8aedb868c5b472d92893b
Sejal-08 <sejalsankhyan2001@gmail.com>
2024-08-20 16:34:31 +0530

    mmm

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4002145086"
+    serviceWorkerVersion: "3289514149"
   }
 });
